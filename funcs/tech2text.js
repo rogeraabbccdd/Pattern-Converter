6e70dcdff111b9debdda2683b4527463d86bc481
rogeraabbccdd <rogeraabbccdd@gmail.com>
2021-02-28 22:34:56 +0800

    Support new tech format

--- funcs/tech2text.js
+++ funcs/tech2text.js
@@ -18,24 +18,55 @@ module.exports = async (file) => {
     bpms.push({ pos: 0, bpm: pattern.patternMetadata.initBpm })
     // parse bpm events
     for (const event of pattern.bpmEvents) {
-      bpms.push({ pos: event.pulse / 5, bpm: event.bpm })
+      bpms.push({ pos: Math.round(event.pulse / 5), bpm: event.bpm })
       bmstimings.push(
         {
           type: 'bpm',
-          beat: parseInt(event.pulse) / 5,
+          beat: Math.round(parseInt(event.pulse) / 5),
           bpm: event.bpm
         }
       )
     }
     // parse notes and collect keysound
     for (const note of pattern.packedNotes) {
-      const data = note.split('|')
-      let attr = 0
-      const pos = data[1] / 5
+      const splitted = note.split('|')
+      let data = {
+        type: '',
+        pulse: '',
+        lane: '',
+        volume: 1,
+        pan: 0,
+        endOfScanString: false,
+        sound: ''
+      }
+      if (splitted[0] === 'E') {
+        // new format
+        // E|{type}|{pulse}|{lane}|{volume}|{pan}|{endOfScanString}|{sound}
+        data.type = splitted[1]
+        data.pulse = splitted[2]
+        data.lane = parseInt(splitted[3])
+        data.volume = splitted[4]
+        data.pan = splitted[5]
+        data.endOfScanString = splitted[6] === '1'
+        data.sound = splitted[7]
+      } else {
+        // old format
+        // {type}|{pulse}|{lane}|{sound}
+        data.type = splitted[0]
+        data.pulse = splitted[1]
+        data.lane = parseInt(splitted[2])
+        data.sound = splitted[3]
+      }
+      if (data.lane > 3) {
+        data.lane += 12
+        if (data.lane > 63) data.lane = 21
+      }
+      const pos = Math.round(data.pulse / 5)
       if (pos + 192 > endpos) {
         endpos = pos + 192
       }
-      switch (data[0]) {
+      let attr = 0
+      switch (data.type) {
         case 'Baisc':
           attr = 0
           break
@@ -52,22 +83,61 @@ module.exports = async (file) => {
           attr = 6
           break
       }
-      const idx = wavs.findIndex(wav => wav.file === data[3])
+      const idx = wavs.findIndex(wav => wav.file === data.sound)
       const id = idx === -1 ? (wavs.length + 1).toString(16).padStart(4, 0).toUpperCase() : wavs[idx].id
-      if (idx === -1) {
-        wavs.push({ file: data[3], id })
+      if (idx === -1 && data.sound.length > 0) {
+        wavs.push({ file: data.sound, id })
       }
-      notes.push({ pos, attr, duration: 6, keysound: id, track: parseInt(data[2]) })
+      notes.push({ pos, attr, duration: 6, keysound: id, track: data.lane, volume: data.volume, pan: data.pan })
+
+      console.log(data.endOfScanString);
+      if (data.endOfScanString && data.lane <= 3) {
+        notes.push({ pos, attr: 0, duration: 6, keysound: '0000', track: data.lane + 4, volume: 1, pan: 0 })
+      }
     }
     // parse long notes
     for (const note of pattern.packedHoldNotes) {
-      const data = note.split('|')
-      let attr = 0
-      const pos = data[2] / 5
+      const splitted = note.split('|')
+      let data = {
+        type: '',
+        pulse: '',
+        duration: '',
+        lane: '',
+        volume: 1,
+        pan: 0,
+        endOfScanString: false,
+        sound: ''
+      }
+      if (splitted[0] === 'E') {
+        // new format
+        // E|{type}|{lane}|{pulse}|{duration}|{volume}|{pan}|{endOfScanString}|{sound}
+        data.type = splitted[1]
+        data.lane = parseInt(splitted[2])
+        data.pulse = splitted[3]
+        data.duration = splitted[4]
+        data.volume = splitted[5]
+        data.pan = splitted[6]
+        data.endOfScanString = splitted[7] === '1'
+        data.sound = splitted[8]
+      } else {
+        // old format
+        // {type}|{lane}|{pulse}|{duration}|{sound}
+        data.type = splitted[0]
+        data.lane = parseInt(splitted[1])
+        data.pulse = splitted[2]
+        data.duration = splitted[3]
+        data.sound = splitted[4]
+      }
+      if (data.lane > 3) {
+        data.lane += 12
+        if (data.lane > 63) data.lane = 21
+      }
+      const pos = Math.round(data.pulse / 5)
       if (pos + 192 > endpos) {
         endpos = pos + 192
       }
-      switch (data[0]) {
+      let attr = 0
+      switch (data.type) {
         case 'Hold':
           attr = 12
           break
@@ -78,17 +148,21 @@ module.exports = async (file) => {
           attr = 11
           break
       }
-      const idx = wavs.findIndex(wav => wav.file === data[4])
+      const idx = wavs.findIndex(wav => wav.file === data.sound)
       const id = idx === -1 ? (wavs.length + 1).toString(16).padStart(4, 0).toUpperCase() : wavs[idx].id
-      if (idx === -1) {
-        wavs.push({ file: data[4], id })
+      if (idx === -1 && data.sound.length > 0) {
+        wavs.push({ file: data.sound, id })
       }
-      notes.push({ pos, attr, duration: data[3] / 5, keysound: id, track: parseInt(data[1]) })
+      notes.push({ pos, attr, duration: Math.round(data.duration / 5), keysound: id, track: data.lane, volume: data.volume, pan: data.pan })
+
+      if (data.endOfScanString && data.lane <= 3) {
+        notes.push({ pos, attr: 0, duration: 6, keysound: '0000', track: data.lane + 4, volume: 1, pan: 0 })
+      }
     }
     // parse drag notes
     for (const note of pattern.packedDragNotes) {
       const data = note.packedNote.split('|')
-      const pos = data[1] / 5
+      const pos = Math.round(data[1] / 5)
       if (pos + 192 > endpos) {
         endpos = pos + 192
       }
@@ -97,16 +171,12 @@ module.exports = async (file) => {
       if (idx === -1) {
         wavs.push({ file: data[3], id })
       }
-      const duration = note.packedNodes[1].split('|')[0] / 5
+      const duration = Math.round(note.packedNodes[1].split('|')[0] / 5)
       notes.push({ pos, attr: 0, duration, keysound: id, track: parseInt(data[2]) })
     }
 
     // arrage notes
     let stringNotes = ''
-    notes.map(note => {
-      if (note.track > 3) note.track += 16
-      return note
-    })
     for (let i = 0; i < 64; i++) {
       stringNotes += '#0 TRACK_START 0 \'\'\r\n'
       // Add bpm change note
@@ -119,9 +189,11 @@ module.exports = async (file) => {
       if (i === 20) {
         stringNotes += `#${endpos} NOTE 0000 127 64 0 6 0\r\n`
       }
-      const noteintrack = notes.filter(note => note.track === i)
+      const noteintrack = notes.filter(note => parseInt(note.track) === i)
       for (const note of noteintrack) {
-        stringNotes += `#${note.pos} NOTE ${note.keysound} 127 64 ${note.attr} ${note.duration} 0\r\n`
+        const vol = note.volume ? Math.round(note.volume * 127) : 127
+        const pan = note.pan ? Math.round((note.pan+1)*(127/2)) : 64
+        stringNotes += `#${note.pos} NOTE ${note.keysound} ${vol} ${pan} ${note.attr} ${note.duration} 0\r\n`
       }
     }
     // caculate song length
