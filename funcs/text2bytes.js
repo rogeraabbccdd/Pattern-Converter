88e79d0792b3bdad05facfb0ed5f9aa534d6bd61
rogeraabbccdd <rogeraabbccdd@gmail.com>
2021-05-15 16:54:12 +0800

    Fix text2bytes file path

--- funcs/text2bytes.js
+++ funcs/text2bytes.js
@@ -1,11 +1,11 @@
 const spawn = require('child_process').spawn
 const path = require('path')
 
-module.exports = (file) => {
+module.exports = (dir, file) => {
   console.log(`Converting ${file} to bytes...`)
   return new Promise((resolve, reject) => {
     const exedir = path.join(process.env.APPDATA, './Pattern-Converter')
-    const child = spawn(path.join(exedir, 'bytes_to_text.exe'), [file])
+    const child = spawn(path.join(exedir, 'bytes_to_text.exe'), [path.join(dir, file)])
     child.on('close', code => {
       reject(new Error('Failed to convert file to bytes.'))
     })
